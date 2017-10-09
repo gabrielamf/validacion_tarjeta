666e0e3c805110a5ed1c4203671c9ab2b224b9d4
gabrielamf <glennygabb@gmail.com>
2017-10-09 12:39:04 -0500

    obtener datos por medio de prompt

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-var str = "4083952015263";
-
+function isValidCard (str){
 str.split("");
 newStr=[];
-for(var i= 0 ; i< str.length ; i++){
-    newStr.push(parseInt(str[i]));
+  for(var i= 0 ; i< str.length ; i++){
+      newStr.push(parseInt(str[i]));
+  }
+
+  var arrayReverse = newStr.reverse();
+  arrayReverse;
+  //[3,6,2,5,1,0,2,5,9,3,8,0,4]
+  var arrayDigitos = [];
+
+  for(var i = 0 ; i < arrayReverse.length ; i++){
+      if( i % 2 !== 0){
+          if ((arrayReverse[i] * 2) >= 10 ){
+              var firstDigit = parseInt((arrayReverse[i] * 2) / 10 );
+              var secondDigit = parseInt((arrayReverse[i] * 2) % 10);
+              var sum = firstDigit + secondDigit;
+              arrayDigitos.push(sum);
+          }else{
+              arrayDigitos.push(arrayReverse[i]);
+          }
+      }else{
+          arrayDigitos.push(arrayReverse[i]);
+      }
+  }
+
+document.write(arrayDigitos);
+
 }
 
-var arrayReverse = newStr.reverse();
-arrayReverse;
-//[3,6,2,5,1,0,2,5,9,3,8,0,4]
-var arrayDigitos = [];
+var str = prompt("Ingrese su número");
 
-for(var i= 0 ; i < arrayReverse.length ; i++){
-    if( i % 2 !== 0){
-        if ((arrayReverse[i] * 2) >= 10 ){
-            var firstDigit = parseInt((arrayReverse[i] * 2) / 10 );
-            var secondDigit = parseInt((arrayReverse[i] * 2) % 10);
-            var sum = firstDigit + secondDigit;
-            arrayDigitos.push(sum);
-        }else{
-            arrayDigitos.push(arrayReverse[i]);
-        }
-    }else{
-        arrayDigitos.push(arrayReverse[i]);
-    }
-}
-
-arrayDigitos;
+isValidCard(str)
